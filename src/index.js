5000c63b397cee598f95230ed7d2d5b8ddceedd9
Andomanova <145564254+Andomanova@users.noreply.github.com>
2023-11-27 21:49:41 +0000

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -11,14 +11,36 @@ function refreshWeather(response) {
 
 
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-    timeElement.innerHTML = "Tuesday 14:49";
+    windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
   
 }
 
+function formatDate(date) {
+   
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
+}
 
 function searchCity(city) {
     // make api call and update the interface 
