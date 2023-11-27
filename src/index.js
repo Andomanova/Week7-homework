15e9b0b233d6b175202e84f61ff8ee7ad994f517
Andomanova <145564254+Andomanova@users.noreply.github.com>
2023-11-27 23:06:59 +0000

    Inject data and time

--- src/index.js
+++ src/index.js
@@ -8,14 +8,17 @@ function refreshWeather(response) {
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
-
-
+   
+    
+    
+    
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
+
   
 }
 
